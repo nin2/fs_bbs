7d6b358f7f2758542a7e096027a8d2ac9a9c004e
Takuya Tominaga <taku@tomiflu.com>
2013-02-23 12:39:01 +0900

    板名をuniquenessにしてたけど解除

--- app/models/board.rb
+++ app/models/board.rb
@@ -2,6 +2,6 @@ class Board < ActiveRecord::Base
   belongs_to :country
   attr_accessible :title, :country_id
 
-  validates :title, :presence => true, :uniqueness => true
+  #validates :title, :presence => true, :uniqueness => true
 end
 
